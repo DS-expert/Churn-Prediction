bececcf472fcc6bc6ec3e4d1be15d43f69e5adf2
DS-expert <workmy.chd@gmail.com>
2026-09-23 16:18:21 +0500

    chore(configuration): Changes in load_config() function in src/utils/config.py, Convert relative paths to absolute paths of paths

--- src/utils/config.py
+++ src/utils/config.py
@@ -16,6 +16,17 @@ def load_config(folder: str="config", file: str="config.yaml") -> dict:
         """
 
     config_path = BASE_DIR / folder / file
+
+    if not config_path.exists():
+        raise FileNotFoundError(f"Configuration file not found at {config_path}")
+
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
+
+    for key, path in config["paths"].items():
+        # convert relative paths to absolute paths
+        if not Path(path).is_absolute():
+            config["paths"][key] = str(BASE_DIR / path)
+    config["project_root"] = str(BASE_DIR)
+    
     return config
